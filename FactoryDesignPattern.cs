7beb34754cd581ec9f5e1d4caab7df3876c71bf8
ecounthan <ngochandevl@gmail.com>
2024-03-27 16:21:36 +0700

    1122

--- FactoryDesignPattern.cs
+++ FactoryDesignPattern.cs
@@ -1,84 +1,106 @@
-// // Creational Design Pattern
-// // tao va tra ve doi tuong lop con tuong ung voi tham so dau vao nhan duoc
-// // dung trong cac ung dung RealTime (thời gian thực) ex: thong bao tin nhan,...
+// Creational Design Pattern
+// tao va tra ve doi tuong lop con tuong ung voi tham so dau vao nhan duoc
+// dung trong cac ung dung RealTime (thời gian thực) ex: thong bao tin nhan,...
 
-// namespace FactoryDesignPattern{
-//       interface ICreditCard{
-//         string GetCardType();
-//         int GetCreditLimit();  
-//         int GetAnnualCharge();
-//     }
-    
-//     internal class MoneyBack: ICreditCard{
-//         public string GetCardType(){
-//             return "MoneyBack";
-//         }
+namespace FactoryDesignPattern
+{
+    interface ICreditCard
+    {
+        string GetCardType();
+        int GetCreditLimit();
+        int GetAnnualCharge();
+    }
 
-//         public int GetCreditLimit(){
-//             return 1;
-//         }
+    internal class MoneyBack : ICreditCard
+    {
+        public string GetCardType()
+        {
+            return "MoneyBack";
+        }
 
-//         public int GetAnnualCharge(){
-//             return 2;
-//         }
-//     }
+        public int GetCreditLimit()
+        {
+            return 1;
+        }
 
-//     internal class Titanium: ICreditCard{
-//         public string GetCardType(){
-//             return "Titanium";
-//         }
+        public int GetAnnualCharge()
+        {
+            return 2;
+        }
+    }
 
-//         public int GetCreditLimit(){
-//             return 3;
-//         }
+    internal class Titanium : ICreditCard
+    {
+        public string GetCardType()
+        {
+            return "Titanium";
+        }
 
-//         public int GetAnnualCharge(){
-//             return 4;
-//         }
-//     }
+        public int GetCreditLimit()
+        {
+            return 3;
+        }
 
-//     internal class Platinum: ICreditCard{
-//         public string GetCardType(){
-//             return "Platinum";
-//         }
+        public int GetAnnualCharge()
+        {
+            return 4;
+        }
+    }
 
-//         public int GetCreditLimit(){
-//             return 5;
-//         }
+    internal class Platinum : ICreditCard
+    {
+        public string GetCardType()
+        {
+            return "Platinum";
+        }
 
-//         public int GetAnnualCharge(){
-//             return 6;
-//         }
-//     }
+        public int GetCreditLimit()
+        {
+            return 5;
+        }
+
+        public int GetAnnualCharge()
+        {
+            return 6;
+        }
+    }
 
 
-//     internal class FactoryBulder{
-//         public static ICreditCard GetCreditCard(string cardType){
-//             ICreditCard objectCard = null;
-//             if(cardType == "MoneyBack"){
-//                 objectCard = new MoneyBack();
-//             }
-//             else if(cardType == "Titanium"){
-//                 objectCard = new Titanium();
-//             }
-//             else if(cardType == "Platinum"){
-//                 objectCard = new Platinum();
-//             }
-//             return objectCard;
-//         }
-//     }
+    internal class FactoryBulder
+    {
+        public static ICreditCard GetCreditCard(string cardType)
+        {
+            ICreditCard objectCard = null;
+            if (cardType == "MoneyBack")
+            {
+                objectCard = new MoneyBack();
+            }
+            else if (cardType == "Titanium")
+            {
+                objectCard = new Titanium();
+            }
+            else if (cardType == "Platinum")
+            {
+                objectCard = new Platinum();
+            }
+            return objectCard;
+        }
+    }
 
-//     internal class exec{
-//         public static void Main(string[] arr){
-//             ICreditCard obj = FactoryBulder.GetCreditCard("Titanium");
-//             if(obj != null){
-//                 Console.WriteLine(obj.GetCardType());
-//                  Console.WriteLine(obj.GetCreditLimit());
-//                   Console.WriteLine(obj.GetAnnualCharge());
-//             }
-//             else
-//                 Console.WriteLine("Invalid Card Type");
-//         }
-//     }
+    internal class exec
+    {
+        public static void Main(string[] arr)
+        {
+            ICreditCard obj = FactoryBulder.GetCreditCard("Titanium");
+            if (obj != null)
+            {
+                Console.WriteLine(obj.GetCardType());
+                Console.WriteLine(obj.GetCreditLimit());
+                Console.WriteLine(obj.GetAnnualCharge());
+            }
+            else
+                Console.WriteLine("Invalid Card Type");
+        }
+    }
 
-// }
+}
